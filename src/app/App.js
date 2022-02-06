02749648ef35cc4431d3cd90f8de8dcfb597a91a
Kjaer <kayer.halil@gmail.com>
2022-02-06 13:08:47 +0100

    Naive implementation of fetching and posting message to the API

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,63 @@
-export function App() {
+import { useState, useEffect } from "react";
+import { getAllMessages, sendMessage } from "../api/chatty";
 
-  return (
-    <h1>Doodle — Chat app challenge</h1>
-  );
+export function App() {
+  const [chatList, setChatList] = useState(null);
+  const [message, setMessage] = useState("");
+
+
+  useEffect(()=>{
+    async function fetchMessages() {
+      const chatMessages = await getAllMessages();
+      setChatList(chatMessages);
+    }
+
+    fetchMessages();
+  }, []);
+
+  async function submitMessage(event) {
+    event.preventDefault();
+
+    const payload = {
+      message,
+      author: "Test Sender",
+    };
+
+    await sendMessage(payload).catch((error) => {
+      throw new Error(`Last message could not send. ${error.toString()}`);
+    });
+  }
+
+  const messageChangeHandler = (event) => {
+    setMessage(event.target.value);
+  };
+
+
+  if (!chatList) {
+    return <h1>Chat history loading...</h1>;
+  }
+
+  return(
+    <section>
+      {chatList.map((chat) => (
+        <article key={chat._id}>
+          <h6>{chat.author}</h6>
+          <p>{chat.message}</p>
+          <time dateTime={new Date(chat.timestamp).toISOString()}>
+            {new Date(chat.timestamp).toLocaleString(
+              Intl.DateTimeFormat().resolvedOptions().locale,
+              { hour: "2-digit", minute: "2-digit" }
+            )}
+          </time>
+        </article>
+      ))}
+
+      <footer>
+        <form onSubmit={submitMessage}>
+          <input type="text" onChange={messageChangeHandler} value={message} />
+          <button type="submit"> Send </button>
+        </form>
+      </footer>
+    </section>
+  )
 }
